a5bcf759eca2efa9c19869778b53d9ec8d324cd0
unknown <paulier.paulier>
2025-07-09 12:34:21 -0300

    Hooks

--- cypress/e2e/loja-ebac/login.cy.js
+++ cypress/e2e/loja-ebac/login.cy.js
@@ -2,8 +2,15 @@
 
 describe('Funcionalidade Login', () => {
 
-  it('Deve fazer login com sucesso', () => {
+  beforeEach(() => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+  });
+
+  afterEach(() => {
+    cy.screenshot()
+  });
+
+  it('Deve fazer login com sucesso', () => {    
     cy.get('#username').type('testepaulier2@teste.com')
     cy.get('#password').type('Aketaton#1906')
     cy.get('.woocommerce-form > .button').click()
